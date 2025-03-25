547f297340f3e74d08f223dfa72a59b2ce66c34b
RaziZaman <145681484+RaziZaman@users.noreply.github.com>
2025-03-25 13:58:23 -0400

    Update PopulateDatabase.java

--- PopulateDatabase.java
+++ PopulateDatabase.java
@@ -2,16 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class PopulateDatabase {
     public static void main(String[] args) {
-        System.out.println("DROP TABLE Student");
-        System.out.println("DROP TABLE Teacher");
-        System.out.println("DROP TABLE Roster");
-        System.out.println("DROP TABLE Teacher_Schedule");
-        System.out.println("DROP TABLE Student_schedule");
-        System.out.println("DROP TABLE Assignments");
-        System.out.println("DROP TABLE Courses");
-        System.out.println("DROP TABLE CourseName");
-        System.out.println("DROP TABLE Departments");
-        System.out.println("DROP TABLE CourseType");
-        System.out.println("DROP TABLE Room");
+        System.out.println("DROP TABLE Student;");
+        System.out.println("DROP TABLE Teacher;");
+        System.out.println("DROP TABLE Roster;");
+        System.out.println("DROP TABLE Teacher_Schedule;");
+        System.out.println("DROP TABLE Student_schedule;");
+        System.out.println("DROP TABLE Assignments;");
+        System.out.println("DROP TABLE Courses;");
+        System.out.println("DROP TABLE CourseName;");
+        System.out.println("DROP TABLE Departments;");
+        System.out.println("DROP TABLE CourseType;");
+        System.out.println("DROP TABLE Room;");
     }
 }
