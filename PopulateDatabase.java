e44d26e2d9aedba29586c1b71f9f4990893d1c67
RaziZaman <145681484+RaziZaman@users.noreply.github.com>
2025-03-25 13:51:40 -0400

    Add files via upload

---
+++ PopulateDatabase.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class PopulateDatabase {
+    public static void main(String[] args) {
+        System.out.println("DROP TABLE Student");
+        System.out.println("DROP TABLE Teacher");
+        System.out.println("DROP TABLE CourseName");
+        System.out.println("DROP TABLE Departments");
+        System.out.println("DROP TABLE CourseType");
+        System.out.println("DROP TABLE Room");
+    }
+}
