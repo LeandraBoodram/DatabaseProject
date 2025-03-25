7e20778925f265b8856809f6c2808f76a5523ddf
BT_4N2_10 <lboodram3318@bths.edu>
2025-03-25 13:55:16 -0400

    drop table

--- PopulateDatabase.java
+++ PopulateDatabase.java
@@ -4,6 +4,10 @@ public class PopulateDatabase {
     public static void main(String[] args) {
         System.out.println("DROP TABLE Student");
         System.out.println("DROP TABLE Teacher");
+        System.out.println("DROP TABLE Roster");
+        System.out.println("DROP TABLE Teacher_Schedule");
+        System.out.println("DROP TABLE Student_schedule");
+        System.out.println("DROP TABLE Courses");
         System.out.println("DROP TABLE CourseName");
         System.out.println("DROP TABLE Departments");
         System.out.println("DROP TABLE CourseType");
