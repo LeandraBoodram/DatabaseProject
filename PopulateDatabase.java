dae3d4d7be2b05770c419a198ca9f98e5ae5d313
LeandraBoodram <145381385+LeandraBoodram@users.noreply.github.com>
2025-03-26 14:00:35 -0400

    Update PopulateDatabase.java

--- PopulateDatabase.java
+++ PopulateDatabase.java
@@ -2,16 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class PopulateDatabase {
     public static void main(String[] args) {
-        System.out.println("DROP TABLE Student;");
-        System.out.println("DROP TABLE Teacher;");
-        System.out.println("DROP TABLE Teacher_Schedule;");
-        System.out.println("DROP TABLE Student_schedule;");
+        System.out.println("DROP TABLE Student_Schedules;");
+        System.out.println("DROP TABLE Teacher_Schedules;");
         System.out.println("DROP TABLE Assignments;");
         System.out.println("DROP TABLE Courses;");
-        System.out.println("DROP TABLE CourseName;");
-        System.out.println("DROP TABLE Departments;");
-        System.out.println("DROP TABLE CourseType;");
         System.out.println("DROP TABLE Room;");
+        System.out.println("DROP TABLE CourseType;");
+        System.out.println("DROP TABLE Departments;");
+        System.out.println("DROP TABLE CourseName;");
+        System.out.println("DROP TABLE Teacher;");
+        System.out.println("DROP TABLE Student;");
         System.out.println("CREATE TABLE Student (Name varchar(255), Id integer PRIMARY KEY );\n" +
                 "CREATE TABLE Teacher (FirstName varchar(255), LastName varchar(255), Id integer PRIMARY KEY );\n" +
                 "CREATE TABLE CourseName (CourseName varchar(255), CourseNameId integer PRIMARY KEY );\n" +
