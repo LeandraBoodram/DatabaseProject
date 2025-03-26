5a345e90464767d0c631ba122eecc18fbfd20d50
RaziZaman <145681484+RaziZaman@users.noreply.github.com>
2025-03-26 13:31:43 -0400

    Update PopulateDatabase.java

--- PopulateDatabase.java
+++ PopulateDatabase.java
@@ -13,7 +13,7 @@ public class PopulateDatabase {
         System.out.println("DROP TABLE Departments;");
         System.out.println("DROP TABLE CourseType;");
         System.out.println("DROP TABLE Room;");
-        System.out.println("CREATE TABLE Student (FirstName varchar(255), LastName varchar(255), Id integer PRIMARY KEY );\n" +
+        System.out.println("CREATE TABLE Student (Name varchar(255), Id integer PRIMARY KEY );\n" +
                 "CREATE TABLE Teacher (FirstName varchar(255), LastName varchar(255), Id integer PRIMARY KEY );\n" +
                 "CREATE TABLE CourseName (CourseName varchar(255), CourseNameId integer PRIMARY KEY );\n" +
                 "CREATE TABLE Departments (Id integer PRIMARY KEY, Name varchar(255), NumOfTeachers integer );\n" +
@@ -23,5 +23,8 @@ public class PopulateDatabase {
                 "CREATE TABLE Assignments (Name varchar(255), CourseId integer, Grade integer, Type varchar(255), StudentId integer, FOREIGN KEY (CourseId) REFERENCES Courses(CourseNameId), FOREIGN KEY (StudentId) REFERENCES Students(Id));\n" +
                 "CREATE TABLE Teacher_Schedules (TeacherId integer, Pd1 integer, Pd2 integer, Pd3  integer, Pd4 integer, Pd5 integer, Pd6 integer, Pd7 integer, Pd8 integer, Pd9 integer, Pd10 integer, FOREIGN KEY (TeacherId) REFERENCES Teacher(Id), FOREIGN KEY (Pd1) REFERENCES Courses(CourseId), FOREIGN KEY (Pd2) REFERENCES Courses(CourseId), FOREIGN KEY (Pd3) REFERENCES Courses(CourseId), FOREIGN KEY (Pd4) REFERENCES Courses(CourseId), FOREIGN KEY (Pd5) REFERENCES Courses(CourseId), FOREIGN KEY (Pd6) REFERENCES Courses(CourseId), FOREIGN KEY (Pd7) REFERENCES Courses(CourseId), FOREIGN KEY (Pd8) REFERENCES Courses(CourseId), FOREIGN KEY (Pd9) REFERENCES Courses(CourseId), FOREIGN KEY (Pd10) REFERENCES Courses(CourseId));\n" +
                 "CREATE TABLE Student_Schedules (StudentId integer, Pd1 integer, Pd2 integer, Pd3 integer, Pd4 integer, Pd5 integer, Pd6 integer, Pd7 integer, Pd8 integer, Pd9 integer, Pd10 integer, FOREIGN KEY (StudentId) REFERENCES Student(Id), FOREIGN KEY (Pd1) REFERENCES Courses(CourseId), FOREIGN KEY (Pd2) REFERENCES Courses(CourseId), FOREIGN KEY (Pd3) REFERENCES Courses(CourseId), FOREIGN KEY (Pd4) REFERENCES Courses(CourseId), FOREIGN KEY (Pd5) REFERENCES Courses(CourseId), FOREIGN KEY (Pd6) REFERENCES Courses(CourseId), FOREIGN KEY (Pd7) REFERENCES Courses(CourseId), FOREIGN KEY (Pd8) REFERENCES Courses(CourseId), FOREIGN KEY (Pd9) REFERENCES Courses(CourseId), FOREIGN KEY (Pd10) REFERENCES Courses(CourseId));\n");
+        for (int i = 1; i <= 5000; i++) {
+            System.out.println("INSERT INTO Student ( Name, Id ) VALUES ( 'Student" + i + "', " + i + ")");
+        }
     }
 }
