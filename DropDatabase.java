53875af06c7762d2eca37aa3557185524c1abe13
RaziZaman <145681484+RaziZaman@users.noreply.github.com>
2025-03-27 13:32:55 -0400

    Update DropDatabase.java

--- DropDatabase.java
+++ DropDatabase.java
@@ -1,1 +1,14 @@
-
+public class PopulateDatabase {
+    public static void main(String[] args) {
+        System.out.println("DROP TABLE Student_Schedules;");
+        System.out.println("DROP TABLE Teacher_Schedules;");
+        System.out.println("DROP TABLE Assignments;");
+        System.out.println("DROP TABLE Courses;");
+        System.out.println("DROP TABLE Room;");
+        System.out.println("DROP TABLE CourseType;");
+        System.out.println("DROP TABLE Departments;");
+        System.out.println("DROP TABLE CourseName;");
+        System.out.println("DROP TABLE Teacher;");
+        System.out.println("DROP TABLE Student;");
+    }
+}
