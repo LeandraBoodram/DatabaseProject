636f969666ce49fd251933772dd14326057567b1
RaziZaman <145681484+RaziZaman@users.noreply.github.com>
2025-04-21 13:47:56 -0400

    Update PopulateDatabase.java

--- PopulateDatabase.java
+++ PopulateDatabase.java
@@ -3,31 +3,27 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-public class Main {
+public class PopulateDatabase {
     public static void main(String[] args) {
         System.out.println("CREATE TABLE Student (Name varchar(255), Id integer PRIMARY KEY );\n" +
                 "CREATE TABLE Teacher (FirstName varchar(255), LastName varchar(255), Id integer PRIMARY KEY, departmentId integer, FOREIGN KEY (departmentId) REFERENCES Departments(Id));\n" +
-                "CREATE TABLE CourseName (CourseName varchar(255), CourseNameId integer PRIMARY KEY );\n" +
+                "CREATE TABLE CourseName (CourseName varchar(255), CourseNameId integer PRIMARY KEY, CourseType varchar(255) );\n" +
                 "CREATE TABLE Departments (Id integer PRIMARY KEY, Name varchar(255), NumOfTeachers integer );\n" +
-                "CREATE TABLE CourseType (Id integer PRIMARY KEY, Name varchar(255) );\n" +
                 "CREATE TABLE Room (Id integer PRIMARY KEY, Name varchar(255) );\n" +
-                "CREATE TABLE Courses (CourseNameId integer, CourseId integer PRIMARY KEY, TypeId integer, RoomId integer, TeacherId integer, FOREIGN KEY (CourseNameId) REFERENCES CourseName(CourseNameId), FOREIGN KEY (TypeId) REFERENCES CourseType(Id), FOREIGN KEY (RoomId) REFERENCES Room(Id), FOREIGN KEY (TeacherId) REFERENCES Teacher(Id) );\n" +
+                "CREATE TABLE Courses (CourseNameId integer, CourseId integer PRIMARY KEY, RoomId integer, TeacherId integer, FOREIGN KEY (CourseNameId) REFERENCES CourseName(CourseNameId), FOREIGN KEY (RoomId) REFERENCES Room(Id), FOREIGN KEY (TeacherId) REFERENCES Teacher(Id) );\n" +
                 "CREATE TABLE Assignments (Name varchar(255), CourseId integer, Grade integer, Type varchar(255), StudentId integer, FOREIGN KEY (CourseId) REFERENCES CourseName(CourseNameId), FOREIGN KEY (StudentId) REFERENCES Student(Id));\n" +
                 "CREATE TABLE Teacher_Schedules (TeacherId integer, Pd1 integer, Pd2 integer, Pd3 integer, Pd4 integer, Pd5 integer, Pd6 integer, Pd7 integer, Pd8 integer, Pd9 integer, Pd10 integer, FOREIGN KEY (TeacherId) REFERENCES Teacher(Id), FOREIGN KEY (Pd1) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd2) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd3) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd4) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd5) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd6) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd7) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd8) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd9) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd10) REFERENCES CourseName(CourseNameId));\n" +
                 "CREATE TABLE Student_Schedules (StudentId integer, Pd1 integer, Pd2 integer, Pd3 integer, Pd4 integer, Pd5 integer, Pd6 integer, Pd7 integer, Pd8 integer, Pd9 integer, Pd10 integer, FOREIGN KEY (StudentId) REFERENCES Student(Id), FOREIGN KEY (Pd1) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd2) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd3) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd4) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd5) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd6) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd7) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd8) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd9) REFERENCES CourseName(CourseNameId), FOREIGN KEY (Pd10) REFERENCES CourseName(CourseNameId));");
 
-        ArrayList<String> teacherNames = getFileData("src/teachernames.csv");
-        ArrayList<String> courseNames = getFileData("src/coursename.csv");
+        ArrayList<String> teacherNames = getFileData("DatabaseProject-main/teachernames.csv");
+        ArrayList<String> courseNames = getFileData("DatabaseProject-main/coursename.csv");
         for (int i = 0; i < courseNames.size(); i++) {
             String[] seperateCourseNames = courseNames.get(i).split(",");
-            //System.out.println("INSERT INTO CourseName ( CourseName, CourseNameId ) VALUES ( " + seperateCourseNames[1] + ", " + (i + 1) + " );");
+            System.out.println("INSERT INTO CourseName ( CourseName, CourseNameId, CourseType ) VALUES ( " + seperateCourseNames[1] + ", " + (i + 1) + ", " + seperateCourseNames[2] + ");");
         }
-        String[] departments = {"Biology", "Chemistry", "CTE", "English", "Health & PE", "LOTE", "Mathematics", "Physics","Social Studies", "Visual & Performing Arts"};
-        //System.out.println("INSERT INTO CourseType ( Id, Name ) VALUES ( 1, 'Regents' );");
-        //System.out.println("INSERT INTO CourseType ( Id, Name ) VALUES ( 2, 'Elective' );");
-        //System.out.println("INSERT INTO CourseType ( Id, Name ) VALUES ( 3, 'AP' );");
+        String[] departments = {"Biology", "Chemistry", "CTE", "English", "ealth & PE", "LOTE", "Mathematics", "Physics","Social Studies", "Visual & Performing Arts"};
         for (int i = 1; i <= departments.length; i++) {
-            System.out.println("INSERT INTO Departments ( Id, Name ) VALUES ( " + i + ", '" + departments[i - 1] + "');");
+            //System.out.println("INSERT INTO Departments ( Id, Name ) VALUES ( " + i + ", '" + departments[i - 1] + "');");
         }
         for (int i = 0; i < teacherNames.size(); i++) {
             String[] seperateNames = teacherNames.get(i).split(" ");
@@ -52,7 +48,7 @@ public class Main {
             }
         }
         for (int i = 0; i < combinations.size(); i++) {
-            System.out.println("INSERT INTO Room ( Id, Name ) VALUES ( " + (i + 1) + ", '" + combinations.get(i) + "' );");
+            //System.out.println("INSERT INTO Room ( Id, Name ) VALUES ( " + (i + 1) + ", '" + combinations.get(i) + "' );");
         }
         ArrayList<Integer> teacheridsP1 = new ArrayList<>();
         ArrayList<Integer> teacheridsP2 = new ArrayList<>();
@@ -75,7 +71,7 @@ public class Main {
                         randomTeacher = (int) (Math.random() * 312 + 1);
                     }
                     teacheridsP1.add(randomTeacher);
-                    System.out.println("INSERT INTO Courses ( CourseNameId, CourseId, TypeId, RoomId, TeacherId ) VALUES ( " + seperateCourseNames[1] + ", " + count + ", " + seperateCourseNames[2]  + " , , , " + randomTeacher + " )");
+                    //System.out.println("INSERT INTO Courses ( CourseNameId, CourseId, RoomId, TeacherId ) VALUES ( " + seperateCourseNames[1] + ", " + count + " , , , " + randomTeacher + " )");
                 } else if (teacheridsP2.size() != 312) {
                     count++;
                     int randomTeacher = (int) (Math.random() * 312 + 1);
@@ -83,7 +79,7 @@ public class Main {
                         randomTeacher = (int) (Math.random() * 312 + 1);
                     }
                     teacheridsP2.add(randomTeacher);
-                    System.out.println("INSERT INTO Courses ( CourseNameId, CourseId, TypeId, RoomId, TeacherId ) VALUES ( " + seperateCourseNames[1] + ", " + count + ", " + seperateCourseNames[2]  + " , , , " + randomTeacher + " )");
+                    //System.out.println("INSERT INTO Courses ( CourseNameId, CourseId, RoomId, TeacherId ) VALUES ( " + seperateCourseNames[1] + ", " + count + " , , , " + randomTeacher + " )");
                 }
             }
         }
